e85521adbd93ae29945d1dda9dc7d25813fed9c2
Chase Olsen <skalolsen@gmail.com>
2023-12-19 11:11:58 -0700

    sushi

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 // SERVER NODE.JS Using ES6 module
 import express from 'express';
 import bodyParser from 'body-parser';
-import https from 'https';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 // Browser Push Notifications
@@ -15,8 +14,8 @@ import skalarsRoute from './routes/skalars.js';
 // Constructing __dirname 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 // App Variables
-// Express
 const app = express();
 const port = process.env.PORT || 4200;
 
@@ -38,9 +37,18 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT, OPTIONS");
     next();
  });
+
+// API routes
 app.use("/api/authorize", authorizeRoute);
 app.use("/api/accountManagement", accountManagementRoute);
 app.use("/api/skalars", skalarsRoute);
+
+// Serve Angular Application - this assumes 'ng build' outputs to 'dist/skalarly-frontend'
+const angularAppPath = join(__dirname, 'dist', 'skalarly-frontend');
+app.use(express.static(angularAppPath));
+app.get('*', (req, res) => {
+  res.sendFile(join(angularAppPath, 'index.html'));
+});
 
 // Server Activation
 app.listen(port, () => {
