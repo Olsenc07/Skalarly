0787dd1efe4e775e7897e4b578a0f1c3e0c762a7
Chase Olsen <skalolsen@gmail.com>
2024-01-10 23:45:05 -0700

    ssr works

--- skalarly-frontend/src/app/assistant-level-code/custom-architecture-aids/services/orientation.service.ts
+++ skalarly-frontend/src/app/assistant-level-code/custom-architecture-aids/services/orientation.service.ts
@@ -12,15 +12,42 @@ screen = computed<boolean>(() => this.orientationState());
   constructor(private ngZone: NgZone, 
     @Inject(PLATFORM_ID) private platformId: Object) {
     if (isPlatformBrowser(this.platformId)) {
+      this.setOrientationState(); 
     window.addEventListener('resize', () => {
       this.ngZone.run(() => {
         const orientationType = window.screen.orientation.type;
-        if(orientationType.includes('landscape')){
-          console.log('my turn')
-        this.orientationState.set(false);
+        switch (orientationType) {
+          case 'landscape-primary':
+          case 'landscape-secondary':
+            this.orientationState.set(false);
+            break;
+          case 'portrait-primary':
+          case 'portrait-secondary':
+            this.orientationState.set(true);
+            break;
+            default:
+              this.orientationState.set(true);
+            break;
         }
       });
     });
   }
   }
+  private setOrientationState(): void {
+    const orientationType = window.screen.orientation.type;
+    switch (orientationType) {
+      case 'landscape-primary':
+      case 'landscape-secondary':
+        this.orientationState.set(false);
+        break;
+      case 'portrait-primary':
+      case 'portrait-secondary':
+        this.orientationState.set(true);
+        break;
+      default:
+        this.orientationState.set(true);
+        break;
+    }
+  }
+
 }
