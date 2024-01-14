395482e65651cd8acfda0cb3d3ee4b8535dfadf6
Chase Olsen <skalolsen@gmail.com>
2024-01-13 18:05:50 -0700

    schema

--- backend/models/basicprovince.js
+++ backend/models/basicprovince.js
@@ -1,5 +1,26 @@
 import mongoose from 'mongoose';
 
+const basicInfoSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  url: { type: String, required: true },
+  studentEnrollment: {
+    fullTimeUndergraduate: { type: Number, default: 0 },
+    partTimeUndergraduate: { type: Number, default: 0 },
+    fullTimeGraduate: { type: Number, default: 0 },
+    partTimeGraduate: { type: Number, default: 0 },
+  },
+  tuitionFees: {
+    canadianStudent: {
+      undergraduate: [{ type: Number }],
+      graduate: [{ type: Number }]
+    },
+    internationalStudent: {
+      undergraduate: [{ type: Number }],
+      graduate: [{ type: Number }]
+    }
+  }
+});
+
 const provinceSchema = new mongoose.Schema({
     province: { type: String, required: true },
     schoolTypes: {
