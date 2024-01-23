9fa0f6e692deaaf4cea12db4d2bc8f8a9431d999
Chase Olsen <skalolsen@gmail.com>
2024-01-23 16:10:17 -0700

    search bar

--- skalarly-frontend/src/app/top-level-code/search-bar/search-bar-expanded/search-bar-expanded.component.ts
+++ skalarly-frontend/src/app/top-level-code/search-bar/search-bar-expanded/search-bar-expanded.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search-bar-expanded',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [MatButtonModule, MatFormFieldModule,
+    MatIconModule, MatInputModule],
   templateUrl: './search-bar-expanded.component.html',
   styleUrl: '../search-bar.component.scss'
 })
