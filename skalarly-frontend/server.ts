78dfeffbc05ee6f097c2ac783f435afbb7241a56
Chase Olsen <skalolsen@gmail.com>
2024-02-07 21:59:15 -0700

    test time

--- skalarly-frontend/server.ts
+++ skalarly-frontend/server.ts
@@ -17,7 +17,6 @@ const DIST_FOLDER = join(process.cwd(), 'dist/skalarly-frontend/');
 console.log('port1', PORT);
 app.set('view engine', 'html');
 app.set('views', DIST_FOLDER);
-app.use(express.static(DIST_FOLDER));
 
 const API_BASE_URL = process.env['BACKEND_API_URL'];
 console.log('API_BASE_URL', API_BASE_URL);
@@ -25,6 +24,14 @@ const apiProxyOptions = {
   target: API_BASE_URL,
   changeOrigin: true,
   followRedirects: true,
+  onError: (err: Error, req: Request, res: Response) => {
+    console.error('Proxy encountered an error:', err);
+    if (!res.headersSent) {
+      res.status(502).json({ error: 'Error occurred while proxying to the API.' });
+    } else {
+      res.end();
+    }
+  }
 };
 app.use('/api/*', (req, res, next) => {
   console.log('API Request Received:', req.baseUrl, req.url);
@@ -52,6 +59,8 @@ app.get('*', async (req: Request, res: Response) => {
     res.status(500).send('Server error');
   }
 });
+app.use(express.static(DIST_FOLDER));
+
 app.listen(PORT, () => {
   console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
