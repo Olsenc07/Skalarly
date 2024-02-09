35ad84bffa110bb5406133f1cc0cfd30fd17a80e
Chase Olsen <skalolsen@gmail.com>
2024-02-08 22:18:16 -0700

    try new url

--- skalarly-frontend/src/app/assistant-level-code/custom-architecture-aids/interceptors/auth-interceptor.ts
+++ skalarly-frontend/src/app/assistant-level-code/custom-architecture-aids/interceptors/auth-interceptor.ts
@@ -19,26 +19,30 @@ export class AuthInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): 
   Observable<HttpEvent<any>> {
-    // if not on signup or login page
-  //   return this.authService.token$.pipe(
-  //     switchMap((token: string | null) => {
-  //       console.log('environment', environment.production);
-  //       const authReq = token ? req.clone({
-  //         setHeaders: {
-  //           Authorization: `Bearer ${token}`
-  //         }
-  //     }): req;
-  //     console.log('authReq', authReq);
-  //     return next.handle(authReq).pipe(
-  //     catchError((error: HttpErrorResponse) => {
-  //       console.error('HTTP error occurred in auth-interceptor', error);
-  //       // Rethrow the error to propagate it to the caller
-  //       throw error
-  //     })
-  //   );
-  // }))
-  // temporary
-  return next.handle(req);
-  
+    if (this.easyAccess(req.url)) {
+      return next.handle(req);
+    } else {
+    return this.authService.token$.pipe(
+      switchMap((token: string | null) => {
+        console.log('environment', environment.production);
+        const authReq = token ? req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`
+          }
+      }): req;
+      console.log('authReq', authReq);
+      return next.handle(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error('HTTP error occurred in auth-interceptor', error);
+        // Rethrow the error to propagate it to the caller
+        throw error
+      })
+    );
+  }))
+}
+  }
+private easyAccess(url: string): boolean {
+  return url.includes('/sign-up') || url.includes('/login') || url.includes('forgot-password')
+  || url.includes('single-feed');
 }
 }
